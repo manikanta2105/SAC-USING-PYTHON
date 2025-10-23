95cb5f76c0bbed0e986a9a576ed1e0dd34b69ea3
Murari Venkat Manikanta <mvenkatmanikanta@gmail.com>
2025-10-23 13:06:32 +0530

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,9 +1,10 @@
-studetails={"name":"adnan",
+studetails={"name":"manikanta",
              "age":"20",
-             "roll no":8020,}
-studetails["sub"]=["LDCA","SAC","OS"]
+             "roll no":4033,}
+studetails["sub"]=["IOT","SAC","OS"]
 print(studetails)
 new_dict = dict.fromkeys(['sub'], None)
 print(new_dict)
 print(studetails['sub'])
+
 
