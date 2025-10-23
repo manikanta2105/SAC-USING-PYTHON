5e91aa9e14870a77930bdd6600333f180482a4a1
Murari Venkat Manikanta <mvenkatmanikanta@gmail.com>
2025-10-23 13:05:45 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,9 @@
+studetails={"name":"adnan",
+             "age":"20",
+             "roll no":8020,}
+studetails["sub"]=["LDCA","SAC","OS"]
+print(studetails)
+new_dict = dict.fromkeys(['sub'], None)
+print(new_dict)
+print(studetails['sub'])
+
